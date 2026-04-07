fc94e204e0ab66f02ef834effdaebd57594a6543
manthapavankumar11@gmail.com <manthapavankumar11@gmail.com>
2026-04-07 22:12:54 +0530

    github actions workflow to publish into pypi

--- src/qql/__init__.py
+++ src/qql/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("qql-cli")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
+
 from .config import DEFAULT_MODEL, QQLConfig, load_config
 from .exceptions import QQLError, QQLRuntimeError, QQLSyntaxError
 from .executor import ExecutionResult, Executor
@@ -5,6 +12,7 @@ from .lexer import Lexer
 from .parser import Parser
 
 __all__ = [
+    "__version__",
     "QQLConfig",
     "QQLError",
     "QQLRuntimeError",
